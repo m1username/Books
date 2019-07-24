b9e96103811dbe5ef8d44abf6ca7bef4521bef7e
oleksandr.dudka <aleksandr.ddk@gmail.com>
2019-07-24 18:41:43 +0300

    modified, tests added

--- src/main/java/com/book/sanchez/PageFactory.java
+++ src/main/java/com/book/sanchez/PageFactory.java
@@ -2,7 +2,6 @@ package com.book.sanchez;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.google.common.collect.ImmutableList;
 
 
--- src/test/java/com/book/sanchez/PageFactoryTest.java
+++ src/test/java/com/book/sanchez/PageFactoryTest.java
@@ -8,10 +8,31 @@ import static org.junit.Assert.assertEquals;
 public class PageFactoryTest {
 
     @Test
-    public void createPageList() {
+    public void createPageListTest() {
 
         List<Page> expectedPageList = ImmutableList.of(new Page("text1"), new Page("text2"), new Page("text3"));
 
         assertEquals(expectedPageList, PageFactory.createPageList(ImmutableList.of("text1", "text2", "text3")));
+    }
+
+    @Test
+    public void getPagesTest() {
+        Page page = new Page("text");
+        assertEquals(ImmutableList.of(page), PageFactory.getPages(page));
+    }
+
+    @Test
+    public void getPages1Test() {
+        Page page = new Page("text");
+        Page page2 = new Page("text");
+        assertEquals(ImmutableList.of(page, page2), PageFactory.getPages(page, page2));
+    }
+
+    @Test
+    public void getPages2Test() {
+        Page page = new Page("text");
+        Page page2 = new Page("text");
+        Page page3 = new Page("text");
+        assertEquals(ImmutableList.of(page, page2, page3), PageFactory.getPages(page, page2, page3));
     }
 }
