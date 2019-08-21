e5f54d04d530f36b82be9bd90e83fd5a2dd5d335
oleksandr.dudka <aleksandr.ddk@gmail.com>
2019-08-21 11:25:06 +0300

    test method stream and test modified

--- src/main/java/com/book/sanchez/StreamTraining.java
+++ src/main/java/com/book/sanchez/StreamTraining.java
@@ -4,10 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamTraining {
-
-    public static String listToString(List<Integer> numbers){
-
-        return numbers.stream().filter((p) -> p % 2 == 0).map(String::valueOf).collect(Collectors.joining(", "));
-
+    public static String listToString(List<Integer> numbers) {
+        return numbers.stream().filter(p -> p % 2 == 0).map(String::valueOf).collect(Collectors.joining(", "));
     }
 }
