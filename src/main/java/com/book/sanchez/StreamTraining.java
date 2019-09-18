fb4026c7cedd31ec0ce99ba374905746cb563d41
oleksandr.dudka <aleksandr.ddk@gmail.com>
2019-09-18 10:18:54 +0300

    test method stream and test modified

--- src/main/java/com/book/sanchez/StreamTraining.java
+++ src/main/java/com/book/sanchez/StreamTraining.java
@@ -1,10 +1,25 @@
 package com.book.sanchez;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class StreamTraining {
     public static String listToString(List<Integer> numbers) {
         return numbers.stream().filter(p -> p % 2 == 0).map(String::valueOf).collect(Collectors.joining(", "));
+    }
+
+    public static Map<Integer, List<Book>> countBooks(List<Book> books) {
+
+        Map<String, List<Book>> result = books.stream().collect(
+                groupingBy(Book::getAuthor, toList()));
+
+        Map<Integer, List<Book>> result2 = result.values().stream().collect(
+                Collectors.toMap(value -> value.size(), value -> value));
+
+        return result2;
     }
 }
--- src/test/java/com/book/sanchez/StreamTrainingTest.java
+++ src/test/java/com/book/sanchez/StreamTrainingTest.java
@@ -1,10 +1,13 @@
 package com.book.sanchez;
 
+import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.List;
 
 public class StreamTrainingTest {
 
@@ -12,5 +15,38 @@ public class StreamTrainingTest {
     public void listToString() {
         String numbers = "2, 6, 8, 200, 480, 500";
         assertEquals(numbers, StreamTraining.listToString(Arrays.asList(2, 5, 6, 7, 8, 200, 301, 403, 480, 500)));
+    }
+
+    @Test
+    public void countBooks() {
+
+        Book book11 = new Book.Builder()
+                .withCover(new Cover())
+                .withAuthor("Shewchenko")
+                .withPages(PageFactory.createPageList(ImmutableList.of("text")))
+                .build();
+
+        Book book22 = new Book.Builder()
+                .withCover(new Cover())
+                .withAuthor("Shewchenko")
+                .withPages(PageFactory.createPageList(ImmutableList.of("text")))
+                .build();
+
+        Book book33 = new Book.Builder()
+                .withCover(new Cover("green", true))
+                .withAuthor("Block")
+                .withPages(PageFactory.createPageList(ImmutableList.of("text", "text1")))
+                .build();
+
+        ArrayList<Book> bookList = new ArrayList<>();
+        bookList.add(book11);
+        bookList.add(book22);
+        bookList.add(book33);
+
+        HashMap<Integer, List<Book>> myHashMap = new HashMap<>();
+        myHashMap.put(2, ImmutableList.of(book11, book22));
+        myHashMap.put(1, ImmutableList.of(book33));
+
+        assertEquals(myHashMap, StreamTraining.countBooks(bookList));
     }
 }
