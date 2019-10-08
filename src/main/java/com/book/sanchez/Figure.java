023b01df6f3e3aed2c37c1dbe236f150772a870d
oleksandr.dudka <aleksandr.ddk@gmail.com>
2019-10-08 18:17:02 +0300

    modified

--- src/main/java/com/book/sanchez/Figure.java
+++ src/main/java/com/book/sanchez/Figure.java
@@ -4,9 +4,26 @@ public abstract class Figure {
     int [] pointA =  new int[2];
     int [] pointB =  new int[2];
 
-    public abstract void move(int distance, String direction);
+    public int getLength() {
+        return pointA[0] - pointB[0];
+    }
+
+    public void move(int distance, String direction){
+        if (direction.equals("UP")) {
+            pointA[1] = pointB[1] = pointA[1] + distance;
+        } else if (direction.equals("DOWN")) {
+            pointA[1] = pointB[1] = pointA[1] - distance;
+        } else if (direction.equals("LEFT")) {
+            pointA[0] = pointB[0] = pointA[0] - distance;
+        } else if (direction.equals("RIGHT")) {
+            pointA[0] = pointB[0] = pointA[0] + distance;
+        }
+    }
 
     public abstract double getArea();
 
-    public abstract void changeSize(int k);
+    public void changeSize(int k){
+        pointA[0] -= k;
+        pointB[0] += k;
+    }
 }
