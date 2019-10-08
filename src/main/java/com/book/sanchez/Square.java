a5a5e97c6dd6cb6dc1c1107e3575305c3dd920c8
oleksandr.dudka <aleksandr.ddk@gmail.com>
2019-10-08 16:48:16 +0300

    Figures added

---
+++ src/main/java/com/book/sanchez/Square.java
@@ -0,0 +1,38 @@
+package com.book.sanchez;
+
+public class Square extends Figure {
+    private int getLength() {
+        return pointA[0] - pointB[0];
+    }
+
+    enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    @Override
+    public void move(int distance, String direction) {
+        if (direction.equals("UP")) {
+            pointA[1] = pointB[1] = pointA[1] + distance;
+        } else if (direction.equals("DOWN")) {
+            pointA[1] = pointB[1] = pointA[1] - distance;
+        } else if (direction.equals("LEFT")) {
+            pointA[0] = pointB[0] = pointA[0] - distance;
+        } else if (direction.equals("RIGHT")) {
+            pointA[0] = pointB[0] = pointA[0] + distance;
+        }
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(getLength(), 2);
+    }
+
+    @Override
+    public void changeSize(int k) {
+        pointA[0] -= k;
+        pointB[0] += k;
+    }
+}
